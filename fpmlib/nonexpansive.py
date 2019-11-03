2260145cb536b1e54bc8987165b727e0c17d98c5
Kazuhiro HISHINUMA <kaz@cs.meiji.ac.jp>
2019-11-03 13:58:59 +0900

    Add Composition class

--- fpmlib/nonexpansive.py
+++ fpmlib/nonexpansive.py
@@ -8,9 +8,9 @@ The following items are automatically loaded when ``fpmlib`` package is imported
 
 import numpy as np
 from typing import Iterable
-from .typing import FixedPointMap, NonexpansiveMap
-from .contracts import check_nonexpansive_map
-__all__ = ['Intersection']
+from .typing import FixedPointMap, FirmlyNonexpansiveMap, NonexpansiveMap
+from .contracts import check_firmly_nonexpansive_map, check_nonexpansive_map
+__all__ = ['Intersection', 'Composition']
 
 
 class Intersection(NonexpansiveMap):
@@ -33,7 +33,7 @@ class Intersection(NonexpansiveMap):
     def __init__(self, maps: Iterable[NonexpansiveMap]):
         maps = tuple(maps)
         if len(maps) < 1:
-            raise ValueError('At least one mapping must be specified.')
+            raise ValueError('At least one mapping must be given.')
         ndim = ([m.ndim for m in maps if isinstance(m, FixedPointMap) and m.ndim] + [None])[0]
         for m in maps:
             check_nonexpansive_map(m, ndim)
@@ -44,5 +44,46 @@ class Intersection(NonexpansiveMap):
     def __call__(self, x):
         return np.average([m(x) for m in self._maps])
 
+    def __contains__(self, x):
+        return all(x in m for m in self._maps)
+
+
+class Composition(NonexpansiveMap):
+    r"""
+    A nonexpansive mapping whose fixed point set coincides with the intersection of the fixed point sets of given nonexpansive mappings.
+    The generated mapping is the composition of given mappings, i.e., for given :math:`T_i\ (i=1,2,\ldots,K)`, it is defined by
+
+    .. math::
+        T:=T_1\circ T_2\circ \ldots \circ T_K.
+
+    Except for the last element, each element in parameter ``maps`` must be ``FirmlyNonexpansiveMap``.
+    If the intersection of the fixed point sets of given nonexpansive mappings is empty, ``__contains__`` operator may not return a correct value.
+    This construction method is based on Propositions 4.9 and 4.49 in [Bauschke2017]_.
+
+    :param maps: A list of nonexpansive mappings.
+    """
+
+    @property
+    def ndim(self):
+        return self._ndim
+    
+    def __init__(self, maps: Iterable[NonexpansiveMap]):
+        maps = tuple(maps)
+        if len(maps) < 1:
+            raise ValueError('At least one mapping must be given.')
+        ndim = ([m.ndim for m in maps if isinstance(m, FixedPointMap) and m.ndim] + [None])[0]
+        for m in maps[:-1]:
+            check_firmly_nonexpansive_map(m, ndim)
+        check_nonexpansive_map(maps[-1], ndim)
+
+        self._maps = maps
+        self._ndim = ndim
+    
+    def __call__(self, x):
+        out = x
+        for m in reversed(self._maps):
+            out = m(out)
+        return out
+    
     def __contains__(self, x):
         return all(x in m for m in self._maps)
--- tests/test_nonexpansive.py
+++ tests/test_nonexpansive.py
@@ -1,6 +1,6 @@
 import numpy as np
 import unittest
-from fpmlib.projections import HalfSpace, Box
+from fpmlib.projections import HalfSpace, Box, Ball
 from fpmlib.nonexpansive import *
 
 
@@ -32,3 +32,46 @@ class TestIntersection(unittest.TestCase):
                 Box(np.array([1, 2])),
                 Box(np.array([1, 2, 3])),
             ])
+
+class TestComposition(unittest.TestCase):
+    def test_1d(self):
+        # p := [-1, 1]
+        p = Composition([HalfSpace(np.array([1]), 1), HalfSpace(np.array([-1]), 1)])
+        self.assertFalse(np.array([-2]) in p)
+        np.testing.assert_almost_equal(p(np.array([-2])), np.array([-1]))
+        self.assertTrue(np.array([-1]) in p)
+        np.testing.assert_equal(p(np.array([-1])), np.array([-1]))
+        self.assertTrue(np.array([0]) in p)
+        np.testing.assert_equal(p(np.array([0])), np.array([0]))
+        self.assertTrue(np.array([1]) in p)
+        np.testing.assert_equal(p(np.array([1])), np.array([1]))
+        self.assertFalse(np.array([2]) in p)
+        np.testing.assert_almost_equal(p(np.array([2])), np.array([1]))
+
+    def test_nd(self):
+        p = Composition([Ball(np.array([-1, 0, 0, 0, 0]), 1), Ball(np.array([1, 0, 0, 0, 0]), 1)])
+        self.assertTrue(np.zeros(5) in p)
+        self.assertFalse(np.array([-1., 0., 0., 0., 0.]) in p)
+        np.testing.assert_almost_equal(p(np.array([-1., 0., 0., 0., 0.])), np.zeros(5))
+        self.assertFalse(np.array([1., 0., 0., 0., 0.]) in p)
+        np.testing.assert_almost_equal(p(np.array([1., 0., 0., 0., 0.])), np.zeros(5))
+
+    def test_ndim_none(self):
+        p = Composition([Box(0), HalfSpace(np.array([1, 1]), 1)])
+        self.assertEqual(p.ndim, 2)
+        self.assertFalse(np.array([-2, -2]) in p)
+        self.assertTrue(np.array([0, 0]) in p)
+        self.assertFalse(np.array([1, 1]) in p)
+
+    def test_ndim_invalid(self):
+        with self.assertRaises(ValueError):
+            Composition([
+                Box(np.array([1, 2])),
+                Box(np.array([1, 2, 3])),
+            ])
+
+    def test_nonexpansive_error(self):
+        nonexp1 = Intersection([Box(0)])
+        nonexp2 = Box(0)
+        with self.assertRaises(ValueError):
+            Composition([nonexp1, nonexp2])
